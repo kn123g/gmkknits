d694401b4d94f57bbc3113ce095392d1c918159c
kn123g <karthiknganesh96@gmail.com>
2020-09-21 22:59:22 +0530

    electron app screen maximied, removed menues

--- main.js
+++ main.js
@@ -1,10 +1,32 @@
 const electron = require('electron');
-//import electron from 'electron';
+const globalShortcut = electron.globalShortcut
+//***//
+function createWindow () {
+	//***//
+
+}
 
 const {app,BrowserWindow} = electron;
 app.on('ready',()=>{
   const mainWindow = new BrowserWindow();
-  mainWindow.loadURL(`file://${__dirname}/View/index.html`);
-  //mainWindow.loadURL('https://www.google.com');
-  console.log('app is ready');
+
+  mainWindow.loadURL(`file://${__dirname}/dist/index.html`);
+
+  globalShortcut.register('f5', function() {
+		console.log('f5 is pressed')
+		mainWindow.reload()
+	});
+	globalShortcut.register('CommandOrControl+R', function() {
+		console.log('CommandOrControl+R is pressed')
+		mainWindow.reload()
+  });
+  // mainWindow.on('closed', function () {
+  //   mainWindow = null
+  // });
+  mainWindow.setMenu(null);
+
+
+  //mainWindow.setFullScreen(true);
+  mainWindow.maximize();
 });
+
